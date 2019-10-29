32fcf2bda705781119b7c2e6e107a251531205ae
Burl Adkins <adkins1393@gmail.com>
2019-10-28 21:06:29 -0400

    POST requests to registration endpoint

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -9,11 +9,25 @@ const AuthApiService = {
             },
             body: JSON.stringify(credentials),
         })
-        .then(res =>
-            (!res.ok)
-                ? res.json().then(e => Promise.reject(e))
-                : res.json()
-        )
+            .then(res =>
+                (!res.ok)
+                    ? res.json().then(e => Promise.reject(e))
+                    : res.json()
+            )
+    },
+    postUser(user) {
+        return fetch(`${config.API_ENDPOINT}/users`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+            .then(res =>
+                (!res.ok) 
+                    ? res.json().then(e => Promise.reject(e))    
+                    : res.json()
+            )
     },
 }
 
